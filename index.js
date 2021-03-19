9bc5067a68374c2afa1ac59a2b50fa61ea95878b
Josie <josiecollinsrc@gmail.com>
2021-03-19 18:11:10 -0500

    added foreign keys

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    port: 3306,
+
+    user: 'root',
+
+    password: 'R00t-Pa$$word',
+    database: 'employee_db',
+});
